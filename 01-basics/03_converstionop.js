f959a780ef46826729a514df66a26337704ba3bf
Syed Minhaj Panjtan <148899506+minhajpanjtan64@users.noreply.github.com>
2024-07-31 12:11:56 +0000

    comparisons in js

--- 01-basics/03_converstionop.js
+++ 01-basics/03_converstionop.js
@@ -44,10 +44,12 @@ let negValue = -value
 // console.log(2/3);
 // console.log(2%3);
 
+// addition of strings
 let str1 = "hello"
 let str2 = "minhaj"
 
 let str3 = str1 + str2
+// operator precedence 
 // console.log(str3);
 
 // console.log("1" + 2);
@@ -60,13 +62,27 @@ let str3 = str1 + str2
 // console.log(+true);
 // console.log(+"");
 
-let num1, num2, num3
 
-num1 = num2 = num3 = 2 + 2
-
-let gameCounter = 100
+// prefix and postfix 
+let gameCounter = 100;
 ++gameCounter;
+// gameCounter++;
+// posfix increment
+// let x = 3;
+// const y = x++;
+// // x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = x2++;
+// // x2 is 4n; y2 is 3n
+// // prefix increment
+// let x = 3;
+// const y = ++x;
+// // x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
 console.log(gameCounter);
 
-// link to study
-// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
