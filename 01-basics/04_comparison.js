34b8595e57b86d62eaf2fb15c09bfbb93d649b91
Syed Minhaj Panjtan <148899506+minhajpanjtan64@users.noreply.github.com>
2024-07-31 12:12:28 +0000

    comparisons in js

---
+++ 01-basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);  returns true value
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1); returns false
+// console.log(2 != 1);
+
+// always  consider having same same dataypes while in comparison
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); //null conveted into zero
+
+console.log(undefined == 0);  
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+// strict check 
+console.log("2" === 2);
